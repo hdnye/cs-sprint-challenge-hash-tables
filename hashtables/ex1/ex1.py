d4573497cee8a416ea3539cccfcdf9ea4acad925
Heather Nye <heatherdnye@gmail.com>
2020-10-11 14:08:34 -0500

    ex1 psuedo code

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,3 +5,20 @@ def get_indices_of_item_weights(weights, length, limit):
     # Your code here
 
     return None
+
+
+"""
+- a f() that adds 2 indeces together & compares them to the limit to find the 
+- pair that equal it. 
+- returns the pairs with the higher value index listed first
+
+first pass solution:
+
+weight = {}
+results = {}  
+    for i in range(length(weights):
+        for j > i in range(length(weights)):
+            if weights[i] + weights[j] == limit:
+                results.append(weights[i][j])
+            else: return None
+"""
