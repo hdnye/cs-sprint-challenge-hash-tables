aad59b42ef1184c5824c345878bc7a627c0f5b6f
Heather Nye <heatherdnye@gmail.com>
2020-10-12 15:11:19 -0500

    ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,17 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
-    return result
+    for i in arrays:
+        for j in i:
+            if j in cache:
+                cache[j] += 1
+            else:
+                cache[j] = 1
+
+    return [j for (j, count) in cache.items() if count == len(arrays)]
+    
 
 
 if __name__ == "__main__":
@@ -15,7 +24,6 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
-
 
 '''
     Return the duplicate ints only in a dict
@@ -28,15 +36,14 @@ if __name__ == "__main__":
     seen = {}
     
     for i in arrays:
-        if i is not in arrays:
+        if i not in arrays:
             seen[i] = 1
         else:
             if seen[i] == 1:
                 dups.update(i)
             seen[i] +=1
+        return dups
+
+
     
-    return dups
-
-
-    for i in arrays:    
 '''
